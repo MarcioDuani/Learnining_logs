2c55cb49fe89030bd774a8d66ee7578b1658ebab
Mduani <marcio_duani@hotmail.com>
2023-12-15 22:20:15 -0300

    adicionando registro de usuários

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,7 +1,47 @@
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def logout_view(resquest):
     logout(resquest)
     return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        # Processa o formulário preenchido
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+
+            # Faz o login do usuário e o redireciona à página inicial
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
+
+#def register(request):
+    #if request.method !='POST':
+      #  form = UserCreationForm()
+
+    #else:
+        #processa o formulario preenchido
+       # form = UserCreationForm(data=request.POST)
+
+       # if form.is_valid():
+      #      new_user = form.save()
+
+        # faz o login do usuário e o redireciona a página inicial
+       # authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+       # login(request, authenticated_user)
+       # return HttpResponseRedirect(reverse('index'))
+
+   # context = {'form': form}
+   # return render(request, 'users/register.html', context)
